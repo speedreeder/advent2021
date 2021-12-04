a928919daa5b5ad15f60098b2c796e22df737110
Alex Reeder <alexgreeder@outlook.com>
2021-12-04 16:26:38 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Kayla!");
+Console.WriteLine("Aaaaaaand, hello Alex!");
+
+var x = "Hello!";
+
+Console.WriteLine(x);
+x = "Goodbye";
+Console.WriteLine(x);
+
