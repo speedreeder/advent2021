74b1266f48fe3f1269e50c7874674e6472214eeb
Alex Reeder <alexgreeder@outlook.com>
2021-12-04 19:19:08 -0500

    Solved problem 1a

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Kayla!");
-Console.WriteLine("Aaaaaaand, hello Alex!");
+﻿var lines = System.IO.File.ReadAllLines(@"./1a.txt");
 
-var x = "Hello!";
+var numIncreased = 0;
+var numDecreased = 0;
+var numEqual = 0;
 
-Console.WriteLine(x);
-x = "Goodbye";
-Console.WriteLine(x);
+for (int i = 0; i < lines.Length; i++)
+{
+    var currentValue = int.Parse(lines[i]);
 
+    if(i == 0) {
+        Console.WriteLine(currentValue + " (N/A - no previous measurement)");
+        continue;
+    }
+    
+    var lastValue = int.Parse(lines[i-1]);
+
+    if (lastValue > currentValue) {
+        Console.WriteLine(currentValue + " (decreased)");
+        numDecreased++;
+        continue;
+    }
+
+    if (lastValue == currentValue) {
+        Console.WriteLine(currentValue + " (equal)");
+        numEqual++;
+        continue;
+    }
+
+    if (lastValue < currentValue) {
+        Console.WriteLine(currentValue + " (increased)");
+        numIncreased++;
+        continue;
+    }
+}
+
+Console.WriteLine($"Total Increased: {numIncreased}");
+Console.WriteLine($"Total Decreased: {numDecreased}");
+Console.WriteLine($"Total Equals: {numEqual}");
