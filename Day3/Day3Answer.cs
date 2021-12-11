a4e985beb41ae852fe922f0713f35e7416d1cc67
Alex Reeder <alexgreeder@outlook.com>
2021-12-10 21:03:32 -0500

    Added day3 part A answer

---
+++ Day3/Day3Answer.cs
@@ -0,0 +1,39 @@
+﻿public class Day3Answer {
+    public string[] Lines {get; set;}
+    public Day3Answer()
+    {
+        Lines = System.IO.File.ReadAllLines(@"./Day3/input.txt");
+    }
+    public void RunPartA() {
+        var gammaRateBinary = "";
+        var episilonRateBinary = "";
+
+        var columns = new string[12];
+
+        for (int i = 0; i < Lines.Length; i++)
+        {
+            for(int j = 0; j < 12; j++){
+                columns[j] = columns[j] + Lines[i][j];
+            }
+        }
+
+        foreach(var column in columns) {
+            if(column.Count(f => f == '1') > column.Count(f => f == '0')) {
+                gammaRateBinary = gammaRateBinary + "1";
+                episilonRateBinary = episilonRateBinary + "0";
+            } 
+            else {
+                gammaRateBinary = gammaRateBinary + "0";
+                episilonRateBinary = episilonRateBinary + "1";
+            }
+        }
+
+        Console.WriteLine($"Final Gamma: {gammaRateBinary} = {Convert.ToInt32(gammaRateBinary, 2)}");
+        Console.WriteLine($"Final Episilon: {episilonRateBinary} = {Convert.ToInt32(episilonRateBinary, 2)}");
+        Console.WriteLine($"{Convert.ToInt32(gammaRateBinary, 2)} x {Convert.ToInt32(episilonRateBinary, 2)} = {Convert.ToInt32(gammaRateBinary, 2) * Convert.ToInt32(episilonRateBinary, 2)}"); 
+    }
+
+    public void RunPartB() {
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 // day1Answer.RunPartA();
 // day1Answer.RunPartB();
 
-var day2Answer = new Day2Answer();
-day2Answer.RunPartA();
-day2Answer.RunPartB();
+// var day2Answer = new Day2Answer();
+// day2Answer.RunPartA();
+// day2Answer.RunPartB();
+
+var day3Answer = new Day3Answer();
+day3Answer.RunPartA();
+// day3Answer.RunPartB();
